0b3517188b3dcccbd37d793a01c682908901713a
SeoMinSeok6672 <aa01055726672@gmail.com>
2025-06-13 09:16:26 +0900

    Update sms_ls.c

--- 명령어만들기/Directory/sms_ls.c
+++ 명령어만들기/Directory/sms_ls.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main() {
+    DIR *dir = opendir(".");
+    struct dirent *entry;
+
+    if (!dir) {
+        perror("opendir");
+        return 1;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_name[0] != '.')
+            printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
+    return 0;
+}
 
