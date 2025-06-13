7179539254e50a5df4ae6e2b04affa497c2dcf65
SeoMinSeok6672 <aa01055726672@gmail.com>
2025-06-13 09:30:01 +0900

    Update sms_ping.c

--- 명령어만들기/Network_Misc/sms_ping.c
+++ 명령어만들기/Network_Misc/sms_ping.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
+        return 1;
+    }
+    char cmd[256];
+    snprintf(cmd, sizeof(cmd), "ping -c 4 %s", argv[1]);
+    system(cmd);
+    return 0;
+}
