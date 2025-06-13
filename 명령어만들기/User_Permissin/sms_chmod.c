39425ab3aae8ab32292858fa2fd0a885dc04871e
SeoMinSeok6672 <aa01055726672@gmail.com>
2025-06-13 09:27:07 +0900

    Update sms_chmod.c

--- 명령어만들기/User_Permissin/sms_chmod.c
+++ 명령어만들기/User_Permissin/sms_chmod.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <mode(8진수)> <file>\n", argv[0]);
+        return 1;
+    }
+
+    mode_t mode = strtol(argv[1], NULL, 8);
+    if (chmod(argv[2], mode) == -1) {
+        perror("chmod 실패");
+        return 1;
+    }
+
+    printf("파일 '%s' 권한을 %o(8진수)로 변경 완료\n", argv[2], mode);
+    return 0;
+}
