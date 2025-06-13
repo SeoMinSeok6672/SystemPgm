f2aea2d7ff172404fd83b937aee49278316731b5
SeoMinSeok6672 <aa01055726672@gmail.com>
2025-06-13 09:31:54 +0900

    Update sms_dnslookup.c

--- 명령어만들기/Network_Misc/sms_dnslookup.c
+++ 명령어만들기/Network_Misc/sms_dnslookup.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <domain>\n", argv[0]);
+        return 1;
+    }
+
+    struct hostent *host = gethostbyname(argv[1]);
+    if (!host) {
+        fprintf(stderr, "DNS 조회 실패\n");
+        return 1;
+    }
+
+    char *ip = inet_ntoa(*(struct in_addr *)host->h_addr);
+    printf("도메인 %s의 IP 주소: %s\n", argv[1], ip);
+    return 0;
+}
