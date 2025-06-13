d9f20d1d1efeaaa939c36a89fc2f1eb830f3011c
SeoMinSeok6672 <aa01055726672@gmail.com>
2025-06-13 09:19:22 +0900

    Update sms_cat.c

--- 명령어만들기/Content/sms_cat.c
+++ 명령어만들기/Content/sms_cat.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    int c;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+
+    fp = fopen(argv[1], "r");
+    if (!fp) {
+        perror("fopen");
+        return 1;
+    }
+
+    while ((c = fgetc(fp)) != EOF)
+        putchar(c);
+
+    fclose(fp);
+    return 0;
+}
